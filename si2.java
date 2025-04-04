f29aa52686a51c5874f35da5a3e27796c2855cd9
sinchan143054 <sinchan143054@gmail.com>
2025-04-04 11:01:39 +0530

    save

---
+++ si2.java
@@ -0,0 +1,16 @@
+package cie;
+
+    
+    public class si2 {
+    public static void main(String[] args) {
+        System.out.println("hi!!!!!!!!!!");
+        System.out.println("hello!!!!!!!!!!!");
+        double p=10;
+        double r=5;
+        double t=2;
+        double sample_interest = (p * r * t) / 100;
+        System.out.println("Simple Interest is: " + sample_interest);
+    }
+    
+}
+
