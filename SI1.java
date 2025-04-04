d5fc71d905252a8364ce6098c0a4ff9ceb82cbd4
sinchan143054 <sinchan143054@gmail.com>
2025-04-04 10:49:04 +0530

    save

--- SI1.java
+++ SI1.java
@@ -2,6 +2,7 @@ package cie;
 
 public class SI1 {
     public static void main(String[] args) {
+        System.out.println("Simple Interest is very usefull in banking ");
         double p=10;
         double r=5;
         double t=2;
